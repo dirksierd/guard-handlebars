f4f94e848defc47c9a61a3afbda35d6d887119b2
Dirk Sierd de Vries <dirksierd@me.com>
2015-08-14 17:02:08 +0200

    twork-specific changes

--- lib/guard/handlebars/template.rb
+++ lib/guard/handlebars/template.rb
@@ -17,12 +17,7 @@ module Guard
 
       def compile
         # TODO Do not assume require.js, but make it possible
-        compiled = "(function() {"
-        compiled << "\n  var #{function} = Handlebars.compile('#{escape(source)}');"
-        compiled << "\n  Handlebars.registerPartial('#{function}', #{function});" if partial?
-        compiled << "\n  return #{function};"
-        compiled << "\n}).call(this);"
-
+        compiled = "Twork.templates.#{function} = Handlebars.compile('#{escape(source)}');"
         compiled
       end
 
